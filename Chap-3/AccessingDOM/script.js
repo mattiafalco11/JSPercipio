d61c547028e3a62795411afe98bb38d0a7ec7b62
Mattia Falco <mattiafalco87@gmail.com>
2022-12-01 16:52:39 +0100

    js

---
+++ Chap-3/AccessingDOM/script.js
@@ -0,0 +1,33 @@
+document.getElementById("numericsection").innerHTML =30;
+
+document.getElementById("textsection").innerHTML = "Name: Jhon";
+
+var a;
+a= 10;
+
+var b, c;
+b = c = 20;
+
+x = a + 30;
+
+console.log("a: "+a);
+console.log("b: "+b);
+console.log("c: "+c);
+console.log("x: "+x);
+
+y = b * c;
+console.log("y: "+y);
+
+StudentName = "Brianna";
+studentname = "Jhon";
+student_name = "David";
+
+console.log("Upper camel case: "+ StudentName);
+console.log("Lower camel case: "+ studentname);
+console.log("Underscore: "+ student_name);
+
+singleQuoteString = 'single quotes';
+backtickString = `back tick`;
+
+console.log('Single quotes: %s', singleQuoteString);
+console.log(`Back tick quotes: ${backtickString}`);
---
+++ Chap-3/SeparateHTML&JS/script.js
@@ -0,0 +1,3 @@
+document.write("Hello world! from js file");
+
+console.log("Hello world! from js file");
