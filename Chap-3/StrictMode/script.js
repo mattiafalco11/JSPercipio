20af13e517032857035a9e63a44d0882bb7fb32d
Mattia Falco <mattiafalco87@gmail.com>
2022-12-01 17:09:52 +0100

    StrictMode

---
+++ Chap-3/StrictMode/script.js
@@ -0,0 +1,11 @@
+"use strict";
+//Con la strict mode dobbiamo dichiarare le variabili obbligatoriamente
+
+var a = 10;
+var private = "Just a string";
+var undefined = "an undefined value";
+b = 50;
+
+console.log("The value of a is %i and b is %i", a, b);
+console.log("Undefined value: "+undefined);
+console.log("Private string: "+private);
